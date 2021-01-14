87f6bfcf53bbdb4a3bc1e7fade568db7dc68bd67
Tyler Gilbert <tyrigi@gmail.com>
2021-01-13 18:22:25 -0800

    First successful build

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
 #include "BleKeypad.h"
 
+BleKeypad bleKeypad;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Starting BLE Service");
+  bleKeypad.begin();
+  Serial.println("BLE Service Started");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(bleKeypad.isConnected()) {
+    Serial.println("Sending 'Hello world'...");
+    bleKeypad.print("Hello world");
+
+    delay(1000);
+  }
+
+  Serial.println("Waiting 5 seconds...");
+  delay(5000);
 }
